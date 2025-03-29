7be248a1a55414c020a3fe8136df8195f39637b5
Raphaelph77 <raphaelzzz72@gmail.com>
2025-03-28 22:28:32 -0300

    botão

---
+++ script.js
@@ -0,0 +1,11 @@
+function moveButton() {
+    const movelButton = document.getElementById('movel');
+    const maxX = window.innerWidth - movelButton.offsetWidth;
+    const maxY = window.innerHeight - movelButton.offsetHeight;
+    
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+
+    movelButton.style.left = randomX + 'px';
+    movelButton.style.top = randomY + 'px';
+}
